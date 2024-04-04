f655f3a86121bc1c31e6744a85e921c5384911dc
mckoyd <dominick.mckoy@gmail.com>
2024-04-04 16:00:14 -0400

    removes examples

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,20 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import ExampleCommonComponent from "../components/ExampleCommonComponent";
 
 const App: React.FC = () => {
   return (
     <main className="app">
       <Routes>
-        <Route
-          path="/home"
-          element={
-            <div>
-              <ExampleCommonComponent exampleProp="Hello React Typescript Webpack World" />
-            </div>
-          }
-        />
-        <Route path="/another-route" element={<h1>I'm on another route</h1>} />
+        <Route path="/" element={<h1>Wolf Social Links Profile</h1>} />
       </Routes>
     </main>
   );
